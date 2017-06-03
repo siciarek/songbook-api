bb843cdc2e4abcfb626aa9184fa4549dc11b862c
Jacek Siciarek <siciarek@gmail.com>
2017-06-03 17:51:19 +0200

    Update profile controller

--- src/UserBundle/Controller/ProfileController.php
+++ src/UserBundle/Controller/ProfileController.php
@@ -6,16 +6,65 @@ use FOS\RestBundle\Routing\ClassResourceInterface;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
+use UserBundle\Entity\User;
 
 /**
  * @RouteResource("Profile", pluralize=false)
  */
 class ProfileController extends FOSRestController implements ClassResourceInterface
 {
-    public function getAction(Request $request)
+    public function getAction()
     {
+        # TODO: change to fos user
+        $criteria = ['username' => $this->getUser()->getUsername()];
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy($criteria)
+            ;
+
         $data = [
-            'message' => 'User profile',
+            'id' => $user->getId(),
+            'firstName' => $user->getFirstName(),
+            'lastName' => $user->getLastName(),
+            'email' => $user->getEmailCanonical(),
+        ];
+
+        return $data;
+    }
+
+    public function postAction() {
+
+        # TODO: change to fos user
+        $criteria = ['username' => $this->getUser()->getUsername()];
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy($criteria)
+        ;
+
+        $data = [
+            'id' => $user->getId(),
+            'firstName' => $user->getFirstName(),
+            'lastName' => $user->getLastName(),
+            'email' => $user->getEmailCanonical(),
+        ];
+
+        return $data;
+    }
+
+    public function putAction() {
+
+        # TODO: change to fos user
+        $criteria = ['username' => $this->getUser()->getUsername()];
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy($criteria)
+        ;
+
+        $data = [
+            'id' => $user->getId(),
+            'firstName' => $user->getFirstName(),
+            'lastName' => $user->getLastName(),
+            'email' => $user->getEmailCanonical(),
         ];
 
         return $data;
